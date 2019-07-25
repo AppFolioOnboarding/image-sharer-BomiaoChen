1682c4fa28e1dbc02b3f1eaa567a537c545346e1
Bomiao Chen <bomiao.chen@appfolio.com>
2019-07-25 16:41:45 -0700

    bc - add create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = 'Post successfully created'
+      head :ok
+    else
+      flash.now[:error] = 'Image URL Invalid'
+      render 'new'
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:link, :caption)
+  end
 end
--- test/controllers/posts_controller_test.rb
+++ test/controllers/posts_controller_test.rb
@@ -8,4 +8,30 @@ class PostControllerTest < ActionDispatch::IntegrationTest
     assert_select '.js-link', 'Link *'
     assert_select '.js-caption', 'Caption'
   end
+
+  test 'invalid post information' do
+    assert_no_difference 'Post.count' do
+      post posts_path, params: { post: {
+        link: 'abc',
+        caption: 'test'
+      } }
+    end
+
+    assert_response :ok
+
+    assert_select '.alert-danger', 'Image URL Invalid'
+    assert_select '.js-link', 'Link *'
+  end
+
+  test 'valid post information' do
+    assert_difference 'Post.count', 1 do
+      post posts_path, params: { post: { link: 'https://cdn.learnenough.com/kitten.jpg',
+                                         caption: '' } }
+    end
+
+    assert_response :ok
+
+    new_post = Post.last
+    assert_equal new_post.link, 'https://cdn.learnenough.com/kitten.jpg'
+  end
 end
