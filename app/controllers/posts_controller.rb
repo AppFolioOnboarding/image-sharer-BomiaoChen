f03afcf18221446be626b5e7b669073823a76886
Bomiao Chen <bomiao.chen@appfolio.com>
2019-08-01 09:29:12 -0700

    bc - add before_action for show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :valid_post, only: [:show]
+
   def new
     @post = Post.new
   end
@@ -14,16 +16,7 @@ class PostsController < ApplicationController
     end
   end
 
-  def show
-    @post = Post.find_by(id: params[:id])
-
-    if @post.present?
-      render 'show'
-    else
-      flash[:error] = 'Image not found'
-      redirect_to new_post_path
-    end
-  end
+  def show; end
 
   def index
     @posts = Post.all.reverse
@@ -33,5 +26,13 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:link, :caption, :tag_list)
+  end
+
+  def valid_post
+    @post = Post.find_by(id: params[:id])
+    return if @post.present?
+
+    flash[:error] = 'Image not found'
+    redirect_to new_post_path
   end
 end
