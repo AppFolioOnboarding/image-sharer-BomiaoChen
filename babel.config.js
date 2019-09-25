1e80799bd964169a01efab82c607fe37111347f0
Bomiao Chen <bomiao.chen@appfolio.com>
2019-09-25 09:27:07 -0700

    bc - package update

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,7 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', { legacy: true }],
     ['@babel/plugin-proposal-class-properties', { loose: true }],
-    '@babel/plugin-transform-object-assign'
+    '@babel/plugin-transform-object-assign',
+    '@babel/plugin-transform-async-to-generator'
   ]
 };
